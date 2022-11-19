2feb97e9e339b17df31379ac35ebf9814f65e499
Lucas Moeskops <lucasmoeskops@gmail.com>
2022-11-19 17:05:01 +0100

    Update 587. Erect the Fence.py

--- 587. Erect the Fence.py
+++ 587. Erect the Fence.py
@@ -5,6 +5,12 @@
 # Comment: lots to learn about goniometry and 2d algorithms
 #
 # Description:
+# You are given an array trees where trees[i] = [xi, yi] represents the location of
+# a tree in the garden.
+# You are asked to fence the entire garden using the minimum length of rope as it
+# is expensive. The garden is well fenced only if all the trees are enclosed.
+# Return the coordinates of trees that are exactly located on the fence perimeter.
+#
 
 def get_slope(x1, y1, x2, y2):
     """ Get integer x and y increments towards x2, y2 """
