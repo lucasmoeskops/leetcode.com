f57864696d10e3515c3125c9d0b7294f09b96281
Lucas Moeskops <lucasmoeskops@gmail.com>
2023-03-29 09:53:18 +0200

    Create 1402. Reducing Dishes.py

---
+++ 1402. Reducing Dishes.py
@@ -0,0 +1,29 @@
+# Solution
+#  Runtime: 96%
+#  Memory usage: 98%
+#
+# Description
+#
+#   A chef has collected data on the satisfaction level of his n dishes. Chef can cook any dish in 1 unit of time.
+#
+#   Like-time coefficient of a dish is defined as the time taken to cook that dish including previous dishes multiplied by its satisfaction level i.e. time[i] * satisfaction[i].
+#
+#   Return the maximum sum of like-time coefficient that the chef can obtain after dishes preparation.
+#
+#   Dishes can be prepared in any order and the chef can discard some dishes to get this maximum value.
+#
+
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        satisfaction.sort(reverse=True)
+        increment = 0
+        total = 0
+
+        for n in satisfaction:
+            if n >= 0 or increment > -n:
+                increment += n
+                total += increment
+            else:
+                break
+        
+        return total
